2b683cd9d2412dbbbf06799d8646c61c4ceff45b
Hamza Arab <hamzaarab@cmail.carleton.ca>
2017-02-04 13:46:37 -0500

    Update analyzeTweets.py
    Added an Approval Rating of Corporations based on sentimental NLP using Indico's API

--- BackEnd/analyzeTweets.py
+++ BackEnd/analyzeTweets.py
@@ -1,12 +1,13 @@
 """
-
 """
-
+import indicoio
 import json
 import pandas as pd
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import re
 import string
+import time
+
 
 def word_in_text(word, text):
     word = word.lower()
@@ -15,9 +16,11 @@ def word_in_text(word, text):
     if match:
         return True
     return False
-    
+
+
 def add_field(df, field):
     df[field] = df[field].apply(lambda tweet: word_in_text(field, tweet))
+
 
 tweets_data_path = 'facebooktweets.txt'
 
@@ -29,8 +32,7 @@ for line in tweets_file:
         tweets_data.append(tweet)
     except:
         continue
-    
-    
+
 tweets = pd.DataFrame()
 
 tweets['lang'] = list(map(lambda tweet: tweet['lang'], tweets_data))
@@ -38,17 +40,45 @@ tweets['text'] = list(map(lambda tweet: tweet['text'], tweets_data))
 
 tweets = tweets[tweets.lang == "en"]
 
-#tweets['stock'] = tweets['text'].apply(lambda tweet: word_in_text('stocks', tweet))
-#tweets['trade'] = tweets['text'].apply(lambda tweet: word_in_text('trade', tweet))
-#tweets['facebook'] = tweets['text'].apply(lambda tweet: word_in_text('facebook', tweet))
+# tweets['stock'] = tweets['text'].apply(lambda tweet: word_in_text('stocks', tweet))
+# tweets['trade'] = tweets['text'].apply(lambda tweet: word_in_text('trade', tweet))
+# tweets['facebook'] = tweets['text'].apply(lambda tweet: word_in_text('facebook', tweet))
 for tweet in tweets['text']:
-    #re.sub(r'[^\x00-\x7f]',r'', tweet)
+    # re.sub(r'[^\x00-\x7f]',r'', tweet)
     tweet = ''.join(filter(lambda x: x in string.printable, tweet))
-    print (tweet)
+    #print(tweet)
+    #print('\n')
 
-#f = open("facebookTweetsText.txt", "w")
-#for tweet in tweets["text"]:
- #   f.write("{}\n".format(tweet))
-    
-#f.close()
-    
+# f = open("facebookTweetsText.txt", "w")
+# for tweet in tweets["text"]:
+#   f.write("{}\n".format(tweet))
+# f.close()
+
+indicoio.config.api_key = '40dc4c756a1582ffe74e549ab29463f2'
+
+tweetsList = tweets['text'].tolist()
+
+start = time.time()
+# batch example
+a= indicoio.sentiment(tweetsList)
+print(a)
+#print(a.all())
+
+aggregate_score = 0
+total = 0
+for i in a:
+    total += i
+    aggregate_score += i - 0.5
+avg = total/len(a)
+
+
+print (aggregate_score)
+approval_rating = avg * 100
+print ("Approval Rating: " + str(approval_rating) + "%")
+if aggregate_score > 0:
+    print("Invest!")
+else:
+    print("Dont Invest!")
+end = time.time()
+
+print (end - start)
