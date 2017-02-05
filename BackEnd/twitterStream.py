0d7119f810c1ea032a8487fdc8c1fe644fccc955
austinoboyle <14arob@queensu.ca>
2017-02-05 02:15:16 -0500

    Add files via upload

--- BackEnd/twitterStream.py
+++ BackEnd/twitterStream.py
@@ -9,6 +9,9 @@ access_token_secret = "ZUUSDOAG1CpwqukZCXZCBvcUrsoYk8j0ZW3HX0p8GgGfH"
 consumer_key = "11JPAvqF8f6rpLy5LI3UG94du"
 consumer_secret = "mPVM202RdkZLxtYt3tvIqnLJpNt1ifsS94X4T40SOIWqE9dTbt"
 
+#f = open("allTweets.json", "w")
+tweetArray = []
+countTweets = 0
 class StdOutListener(StreamListener):
     def on_data(self, data):
         print (data)
@@ -17,13 +20,20 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print("Error {}".format(status))
 
-def getCompanyTweets():
+def getTweets():
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    stream.filter(languages=["en"], track=["a", "the", "i", "you", "u"])
-    #This line filters Twitter Streams to capture most tweets written in english
+    stream.filter(languages=["en"], track=["donald trump", "Donald Trump"])
 
 if __name__ == '__main__':
-    getCompanyTweets()
+    
+    getTweets()
+    
+"""topics_list = ["facebook", "tesla", "donald trump", 
+        "hilary clinton", "twitter", "canada", "isis", 
+        "syria", "brexit" 
+        "snapchat", "instagram","reddit", 
+        "police", "dogs", "cats"]"""
+    
